72da0e9ba65410035df636bcb950a25c8ac2cc81
Candrese <candrese@student.42Heilbronn.de>
2025-05-20 06:56:53 +0200

    current Makefile and main, given from subject. lets work on some more thorough testing now

---
+++ ex03/main.cpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: candrese <candrese@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/20 02:53:38 by candrese          #+#    #+#             */
+/*   Updated: 2025/05/20 06:15:21 by candrese         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "AMateria.hpp"
+#include "Ice.hpp"
+#include "Cure.hpp"
+#include "Character.hpp"
+#include "MateriaSource.hpp"
+#include "ICharacter.hpp"
+#include "IMateriaSource.hpp"
+#include <iostream>
+
+
+//main function from subject
+
+int main()
+{
+IMateriaSource* src = new MateriaSource();
+src->learnMateria(new Ice());
+src->learnMateria(new Cure());
+ICharacter* me = new Character("me");
+AMateria* tmp;
+tmp = src->createMateria("ice");
+me->equip(tmp);
+tmp = src->createMateria("cure");
+me->equip(tmp);
+ICharacter* bob = new Character("bob");
+me->use(0, *bob);
+me->use(1, *bob);
+delete bob;
+delete me;
+delete src;
+return 0;
+}
+ 
+
+//more thorough tests
